fa6a9dde61be0b6c8ecdd3a3574c9e576fa01bbc
Rong <idskof@sina.cn>
2021-01-20 22:42:03 +0800

    removeDuplicates

--- threeSum.py
+++ threeSum.py
@@ -2,20 +2,25 @@ class Solution(object):
     def threeSum(self, nums):
         n = len(nums)
         res = []
-        for i in range(n-2):
-            for j in range(i+1,n-1):
-                for k in range(j+1,n):
+        rm = []
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
                     if nums[i] + nums[j] + nums[k] == 0:
-                        res.append([nums[i] ,nums[j] , nums[k]])
+                        rr = {nums[i], nums[j], nums[k]}
+                        if rr not in rm:
+                            rm.append(rr)
+                            res.append([nums[i], nums[j], nums[k]])
                         break
         return res
 
 
-def threeSum(arr):
-    pass
+def threeSum(nums):
+    nums.sort()
+
 
 
 so = Solution()
-nums = [-1,0,1,2,-1,-4]
+nums = [-1, 0, 1, 2, -1, -4]
 res = so.threeSum(nums)
 print(res)
